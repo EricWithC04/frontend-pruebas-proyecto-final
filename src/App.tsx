08e115c4ccbd7b924c967b2c4f2ae1957e5faadf
EricWithC04 <negativegame117@gmail.com>
2024-06-06 20:02:15 -0300

    feat: react-router-dom config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
 import MainPage from './components/MainPage/MainPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ThemePage from './components/ThemePage/ThemePage'
 
 function App() {
   return (
     <>
-      <MainPage></MainPage>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/example" element={<ThemePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
