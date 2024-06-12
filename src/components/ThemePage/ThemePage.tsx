10bd2ed0f76cde30709df1277482cf2ec3199f1c
EricWithC04 <negativegame117@gmail.com>
2024-06-11 21:26:31 -0300

    feat: extend each exercise description

--- src/components/ThemePage/ThemePage.tsx
+++ src/components/ThemePage/ThemePage.tsx
@@ -20,39 +20,58 @@ const ThemePage = () => {
         })
     }, [])
 
-    const [viewExercises, setViewExercises] = useState(false)
-
-    const exampleExercises = [
+    const [exampleExercises, setExampleExercises] = useState([
         {
+            id: 1,
             name: 'Tipos de Datos',
+            extended: false,
             description: 'Aprende sobre los diferentes tipos de datos en Python, como enteros, flotantes, cadenas de texto y booleanos. Este ejercicio te ayudará a entender cómo trabajar con cada tipo de dato y cuándo utilizarlos en tus programas.'
         },
         {
+            id: 2,
             name: 'Estructuras de Datos',
+            extended: false,
             description: 'Explora las principales estructuras de datos en Python, incluyendo listas, tuplas, conjuntos y diccionarios. Aprende cómo almacenar, acceder y manipular colecciones de datos eficientemente.'
         },
         {
+            id: 3,
             name: 'Variables',
+            extended: false,
             description: 'Entiende el concepto de variables en Python y cómo se utilizan para almacenar datos. Este ejercicio te enseñará cómo declarar, asignar y modificar variables, así como las reglas de nomenclatura y el alcance de las mismas.'
         },
         {
+            id: 4,
             name: 'Funciones',
+            extended: false,
             description: 'Descubre cómo definir y utilizar funciones en Python. Aprende sobre parámetros, argumentos, valores de retorno y el concepto de alcance de variables dentro de las funciones. Este ejercicio te ayudará a estructurar tu código de manera más modular y reutilizable.'
         },
         {
+            id: 5,
             name: 'Condicionales',
+            extended: false,
             description: 'Aprende a tomar decisiones en tu código utilizando declaraciones condicionales como if, elif y else. Este ejercicio te enseñará a controlar el flujo de ejecución basándote en diferentes condiciones y a implementar lógica condicional compleja.'
         },
         {
+            id: 7,
             name: 'Ciclos o Bucles',
+            extended: false,
             description: 'Familiarízate con los bucles en Python, como for y while. Aprende cómo repetir bloques de código múltiples veces y cómo utilizar los bucles para iterar sobre secuencias de datos. Este ejercicio también cubre el uso de declaraciones de control como break y continue.'
         },
         {
+            id: 8,
             name: 'Modulos',
+            extended: false,
             description: 'Descubre cómo organizar y reutilizar tu código mediante módulos en Python. Aprende a importar módulos estándar y crear tus propios módulos, facilitando así la gestión y el mantenimiento de proyectos de programación más grandes.'
-        },
-        
-    ]
+        },  
+    ])
+
+    const handleExtendExercise = (id: any) => {
+        const newExampleExercises = exampleExercises.map((exercise) => {
+            if (exercise.id == id) exercise.extended = !exercise.extended
+            return exercise
+        })
+        setExampleExercises(newExampleExercises)
+    }
 
     if (details.length) {
         return (
@@ -83,10 +102,10 @@ const ThemePage = () => {
                     {
                         exampleExercises.map((exercise: any) => (
                                 <div className='exercise-item-container'>
-                                    <div className='theme-item expanded exercise w-100' onClick={() => setViewExercises(!viewExercises)}>
+                                    <div className='theme-item expanded exercise w-100' onClick={() => handleExtendExercise(exercise.id)}>
                                         <p>{exercise.name}</p>
                                     </div>
-                                    <div className={`theme-item-description w-100 ${viewExercises ? 'yes-display' : 'no-display'}`}>
+                                    <div className={`theme-item-description w-100 ${exercise.extended ? 'yes-display' : 'no-display'}`}>
                                         <p>{exercise.description}</p>
                                         <button>Ir al Ejercicio</button>
                                     </div>
